75fd3662fc1c032520a9608eb359d8276dbbad57
Chee Yeo <ckyeo.1@gmail.com>
2019-10-28 17:22:10 +0000

    Update to tf 2.0

--- chapter16_1x1_convolutions/README.md
+++ chapter16_1x1_convolutions/README.md
@@ -2,3 +2,10 @@
 
 Examples from chapter 16 of Deep Learning For Computer Vision
 
+## Notes
+
+* The 1x1 filter can be used to create a linear projection of a stack of feature maps.
+
+* The projection created by a 1x1 filter can act like channel-wise pooling and be used for dimensionality reduction.
+
+* The projection created by a 1x1 filter can also be used directly or be used to increase the number of feature maps in a model.
--- chapter16_1x1_convolutions/example1.py
+++ chapter16_1x1_convolutions/example1.py
@@ -1,7 +1,11 @@
-from keras.models import Sequential
-from keras.layers import Conv2D
+# Example of applying 1x1 filter to create a projection of the feature maps 
+# Referred to as channel-wise pooling
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
 
 model = Sequential()
 model.add(Conv2D(512, (3,3), padding='same', activation='relu', input_shape=(256, 256, 3)))
+# Apply 1x1 projection here; note that the num of filters remain the same
 model.add(Conv2D(512, (1,1), activation='relu'))
 model.summary()
